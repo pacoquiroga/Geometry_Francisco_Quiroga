ed5413ebe605313e2a93acc4db2d42d170fc0eab
Francisco Quiroga <pacoquiroga33@gmail.com>
2022-11-23 08:29:02 -0500

    Geometry Francisco Quiroga

---
+++ FRANCISCO_SEBASTIAN_QUIROGA_BENITEZ/Geometry/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle rectangle = new Rectangle(6, 5);
+            Square square = new Square(10);
+            Circle circle = new Circle(7);
+
+            IShape[] shapes = { rectangle, square, circle };
+
+            foreach (IShape shape in shapes)
+            {
+                Console.WriteLine(shape.CalculateArea());
+                Console.WriteLine(shape.CalculatePerimeter());
+
+            }
+
+        }
+    }
+}
